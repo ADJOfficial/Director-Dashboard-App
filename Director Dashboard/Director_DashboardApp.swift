c54463c1e5cfd7bbe3028e0bcebf7568fad12853
ADJOfficial <ArsalanDaud098@gamil.com>
2024-01-06 06:48:34 +0500

    Initial Commit

---
+++ Director Dashboard/Director_DashboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Director_DashboardApp.swift
+//  Director Dashboard
+//
+//  Created by ADJ on 06/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct Director_DashboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
